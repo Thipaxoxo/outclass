3f4922f9bc1ee9300c4a393524611383962e67b1
Thipaxoxo <prathipa@airasia.com>
2024-09-11 16:35:20 +0800

    Update sales-generate-predict.py

--- sales-generate-predict.py
+++ sales-generate-predict.py
@@ -12,10 +12,10 @@ st.sidebar.header('User Input Parameters')
 def user_input_features():
     TV= st.sidebar.slider('TV',0.0,296.4,149.75)
     Radio = st.sidebar.slider('Radio',0.0,49.6,22.9)
-    NewsPaper= st.sidebar.slider('NewsPaper',0.0,114.0,25.75)
+    Newspaper= st.sidebar.slider('Newspaper',0.0,114.0,25.75)
     data = {'TV': TV,
             'Radio': Radio,
-            'NewsPaper': NewsPaper}
+            'Newspaper': Newspaper}
     features = pd.DataFrame(data, index=[0])
     return features
 
